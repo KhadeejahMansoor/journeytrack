448539be9d041c5f244870c9cd937160e55a71ca
Khadeejah <deejahmansoor@gmail.com>
2024-11-20 22:28:27 +0500

    modify default.json

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI'); // Get MongoDB URI from the configuration file
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    // Attempt to connect to MongoDB
     await mongoose.connect(db, {
-      useNewUrlParser: true, // Use the new MongoDB connection string parser
-      useUnifiedTopology: true, // Opt into the MongoDB driver's unified topology layer
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
-    // Exit the process with a failure code
     process.exit(1);
   }
 };
 
-module.exports = connectDB; // Export the function for use in server.js
+module.exports = connectDB;
