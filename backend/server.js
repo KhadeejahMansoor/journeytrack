3e562c392006570203f9dbdbfebd8a47af5ad0ac
Khadeejah <deejahmansoor@gmail.com>
2024-11-20 22:18:38 +0500

    Update server.js and db.js for database connection; modify routes and models for injection and patient management

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,29 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
-const cors = require('cors');
-app.use(cors());
 
+// Middleware
+app.use(cors()); // Enable CORS
+app.use(express.json({ extended: false })); // Parse JSON requests
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
 
 // Connect Database
 connectDB(); // Connect to MongoDB Atlas
 
-// Init Middleware
-app.use(express.json({ extended: false })); // Parse JSON requests
-
 // Define Routes
-app.use('/api/users', require('./routes/api/patients')); // User-related routes
-app.use('/api/auth', require('./routes/api/injections')); // Authentication routes
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/api/patients', require('./routes/patients')); // Patient-related routes
+app.use('/api/injections', require('./routes/injections')); // Injection-related routes
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder for React
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
