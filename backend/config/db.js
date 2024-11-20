edaa346f5cd0962720e6e383db52cc3c82f7e6fd
Khadeejah <deejahmansoor@gmail.com>
2024-11-20 22:54:01 +0500

    modify default.json db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,8 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(db);
+
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
